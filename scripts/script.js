89c33ee13c46e494555848832bbf745f24a22535
fusakoo <obatafusako@gmail.com>
2020-08-20 15:55:47 -0700

    Get operate & update result working

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,36 @@
+let result = 0;
+const resultValue = document.querySelector('#result');
+
 function add (a, b) {
-	return a + b;
+	result = a + b;
 }
 
 function subtract (a, b) {
-	return a - b;
+	result = a - b;
 }
 
 function multiply (a, b) {
-    return a * b;
+    result = a * b;
 }
 
 function divide (a, b) {
-    return a / b;
+    result = a / b;
+}
+
+function operate (operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a,b);
+    } else if (operator === '*') {
+        return multiply(a,b);
+    } else if (operator === '/') {
+        return divide(a,b);
+    } else {
+        return 'Error';
+    }
+}
+
+function updateScreen() {
+    resultValue.innerHTML = result;
 }
